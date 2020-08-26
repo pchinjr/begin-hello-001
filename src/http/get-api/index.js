e9643776a4f36537754306248bfb99bb1cbff6b4
Paul Chin Jr <pchinjr@gmail.com>
2020-08-26 18:59:04 -0400

    missing comma

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,9 +1,10 @@
 exports.handler = async function(req) {
+
   return {
     "statusCode": 200,
     "headers": {
       "content-type":"text/html; charset=utf-8"
-    }
+    },
     "body": '<h1>praise cage</h1>'
   }
 }
