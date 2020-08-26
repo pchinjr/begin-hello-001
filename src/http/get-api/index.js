9236f6e69915a731ddb80f3f3874e56b7767c518
Paul Chin Jr <pchinjr@gmail.com>
2020-08-26 18:52:03 -0400

    Update index.js

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,3 +1,9 @@
 exports.handler = async function(req) {
-  return 'praise cage'
+  return {
+    "statusCode": 200,
+    "headers": {
+      "content-type":"text/html; charset=utf-8"
+    }
+    "body": '<h1>praise cage</h1>'
+  }
 }
